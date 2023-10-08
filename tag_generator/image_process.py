5a7d2620b5d36ebc4ad28314467c3b82a0b7beb2
Sai Shashank <shashank.sai20@gmail.com>
2023-10-07 21:45:13 -0700

    Added ocr code

--- tag_generator/image_process.py
+++ tag_generator/image_process.py
@@ -1,2 +1,9 @@
-def image_process():
-    pass
+from PIL import Image
+import pytesseract
+def image_process(image_path):
+    image = Image.open(image_path)
+    
+    # Understand the image
+
+    # Get text from image
+    text = pytesseract.image_to_string(image)
